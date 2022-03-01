83c3e1f5acb663e8f4eddbe5c6a2657dfeeeaca6
Maycon Santos <mayconalves.santos14@gmail.com>
2022-03-01 17:57:54 -0300

    adding coins

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -9,6 +9,35 @@ export default class Main extends Phaser.Scene {
   private arrow!: Phaser.Types.Input.Keyboard.CursorKeys;
   private walls!: Phaser.Physics.Arcade.StaticGroup;
 
+  private coin!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+
+  private score!: number;
+  private scoreLabel!: Phaser.GameObjects.Text;
+
+  private update_coin_position() {
+    let positions = [
+      { x: 140, y: 60 },
+      { x: 360, y: 60 },
+      { x: 60, y: 140 },
+      { x: 440, y: 140 },
+      { x: 130, y: 300 },
+      { x: 370, y: 300 },
+    ];
+
+    positions = positions.filter((position) => position.x !== this.coin.x);
+
+    const { x, y } = Phaser.Math.RND.pick(positions);
+
+    this.coin.setPosition(x, y);
+  }
+
+  private take_coin() {
+    this.score += 5;
+    this.scoreLabel.setText("Score: " + this.score.toString());
+
+    this.update_coin_position();
+  }
+
   private move_player() {
     if (this.arrow.left.isDown) {
       this.player.body.setVelocityX(-200);
@@ -47,7 +76,9 @@ export default class Main extends Phaser.Scene {
   }
 
   private player_die() {
-    this.scene.start("GameScene");
+    if (this.player.y > 340 || this.player.y < 0) {
+      this.scene.start("GameScene");
+    }
   }
 
   preload() {
@@ -55,23 +86,27 @@ export default class Main extends Phaser.Scene {
     this.load.image("player", "player.png");
     this.load.image("wallVertical", "wallVertical.png");
     this.load.image("wallHorizontal", "wallHorizontal.png");
+    this.load.image("coin", "coin.png");
   }
 
   create() {
+    this.score = 0;
+    this.scoreLabel = this.add.text(30, 25, "Score: 0", {
+      font: "18px Arial",
+    });
+    this.coin = this.physics.add.sprite(60, 130, "coin");
     this.player = this.physics.add.sprite(250, 170, "player");
     this.player.body.setGravityY(500);
-
     this.arrow = this.input.keyboard.createCursorKeys();
-
     this.create_world();
   }
 
   update(time: number, delta: number) {
     this.physics.collide(this.player, this.walls);
-    this.move_player();
-
-    if (this.player.y > 340 || this.player.y < 0) {
-      this.player_die();
+    if (this.physics.overlap(this.player, this.coin)) {
+      this.take_coin();
     }
+    this.move_player();
+    this.player_die();
   }
 }
